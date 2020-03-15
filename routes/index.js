22babaf6568739c56498827ea17ae606f24f87ab
Daniela <daniela.arroyo27@gmail.com>
2020-03-15 16:14:30 -0500

    index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
+let db = require('../models/database');
 router.get('/', (req, res) => {
-  res.render('index.ejs');
+  db.query("SELECT * FROM blogs")
+    .then((results) => {
+      // [{title:value, author_id:value, category_id: value, body:value, date_pub: value}, {}, {}, {}]
+      res.render('index.ejs', {
+        blogs: results
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send('error.')
+    })
 });
-
 module.exports = router;
 
